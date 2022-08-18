3f82002d9c410170bedc7e2cbab75b947109a65e
wilsonshaw <xiaowenchaode@qq.com>
2022-08-19 00:08:28 +0800

    Changed the header file of Engine

--- include/Engine.h
+++ include/Engine.h
@@ -4,9 +4,16 @@ class Engine: public EngineInterface{
 public:
     virtual void gameMain(GameObject* gameInit, GameObject* hundleEvents, 
     GameObject* update, GameObject* render);
-    virtual bool getisIfRunning();
+    virtual bool getIsIfRunning();
+    virtual bool getIsIfRunning(bool set);
+    virtual bool setIsIfHundleEvents(bool set);
+    virtual bool setIsIfUpdate(bool set);
+    virtual bool setIsIfRender(bool set);
 private:
     static bool isIfRunning;
+    static bool isIfHundleEvents;
+    static bool isIfUpdate;
+    static bool isIfRender;
     static bool GameInit(GameObject* object);
     void hundleEvents(GameObject* object);
     void update(GameObject* object);
--- include/EngineInterface.h
+++ include/EngineInterface.h
@@ -13,7 +13,8 @@ class EngineInterface{
 public:
     virtual void gameMain(GameObject* gameInit, GameObject* hundleEvents, 
     GameObject* update, GameObject* render) = 0;
-    virtual bool getisIfRunning() = 0;
+    virtual bool getIsIfRunning() = 0;
+    virtual ~EngineInterface(){}
 };
 
 extern "C"{
