a747428ca79d114b19c56204d31fa0acdb1adc05
wilsonshaw <xiaowenchaode@qq.com>
2022-09-04 20:38:28 +0800

    remove the Qt dll:Frame

--- src/Frame/Frame.h
+++ src/Frame/Frame.h
@@ -3,28 +3,3 @@
 #else
     #define Frame_API __declspec(dllimport)
 #endif
-
-#include <QObject>
-#include <QWidget>
-#include <QLayout>
-#include <QString>
-#include <QPushButton>
-
-class Frame_API Frame: public QWidget
-{
-    Q_OBJECT
-private:
-    QPushButton* startButton;
-    QPushButton* closeButton;
-    QVBoxLayout* buttonsLayout;
-    QHBoxLayout* winLayout;
-//Private methods
-    void initButton();
-    void initLayout();    
-public:
-    Frame(Frame* parents = nullptr, Qt::WindowFlags f = Qt::WindowFlags());
-    ~Frame();
-signals:
-
-public slots:
-};
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,7 @@
 #include "Engine/Engine.h"
 #include "Frame/Frame.h"
 
-class Engine_API Engine;
-class Frame_API Frame;
-
 int main(int argc, char** argv){ 
     QApplication app = QApplication(argc, argv);
-    Frame mainWin;
-    mainWin.show();
     return app.exec();
 }
