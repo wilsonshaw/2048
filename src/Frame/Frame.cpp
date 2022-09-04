bbe49c55b4fef3f368b8b8e501e05939b2804cca
wilsonshaw <xiaowenchaode@qq.com>
2022-09-04 23:59:35 +0800

    get new dll

--- src/Frame/Frame.cpp
+++ src/Frame/Frame.cpp
@@ -1,1 +1,11 @@
 #include "Frame.h"
+
+Frame::Frame(Frame* parent, Qt::WindowFlags f): QWidget(parent, f)
+{
+
+}
+
+Frame::~Frame()
+{
+
+}
--- src/Frame/Frame.h
+++ src/Frame/Frame.h
@@ -3,3 +3,19 @@
 #else
     #define Frame_API __declspec(dllimport)
 #endif
+
+#include <QWidget>
+
+class Frame_API Frame:public QWidget
+{
+    Q_OBJECT
+private:  
+
+public:
+    Frame(Frame* parent = nullptr, Qt::WindowFlags f = Qt::WindowFlags());
+    ~Frame();
+signals:
+
+public slots:
+
+};
